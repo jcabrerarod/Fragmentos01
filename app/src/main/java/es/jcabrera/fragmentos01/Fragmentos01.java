a8146229ca1f67fa8ed58c5d057eab34ad694243
SARANAJA <jacinto@iesvirgendelcarmen.com>
2018-10-08 19:27:42 +0200

    Initial commit

---
+++ app/src/main/java/es/jcabrera/fragmentos01/Fragmentos01.java
@@ -0,0 +1,13 @@
+package es.jcabrera.fragmentos01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fragmentos01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragmentos01);
+    }
+}
